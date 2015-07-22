01899744ed9ae9a93ce7ecc196d62fd7215ea7da
Catalin Boruga <catalinboruga@yahoo.com>
2015-07-22 08:56:46 +0200

    Add authorization

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -13,5 +13,15 @@ describe "Viewing the list of users" do
     expect(page).to have_link(user2.name)
     expect(page).to have_link(user3.name)
   end
+  it "shows the user's details" do
+    user = User.create!(user_attributes)
+
+    sign_in(user)
+
+    visit user_url(user)
+
+    expect(page).to have_text(user.name)
+    expect(page).to have_text(user.email)
+  end
 
 end
